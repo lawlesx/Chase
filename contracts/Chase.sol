375b4331ad2c7c8a7ed063cb570d73ced7fb01a7
Aniruddha Sil <welcometohellv@gmail.com>
2023-12-08 23:30:29 +0530

    feat(Chase.sol): :sparkles: set number of claims and divide amount

--- contracts/Chase.sol
+++ contracts/Chase.sol
@@ -2,25 +2,50 @@
 pragma solidity ^0.8.0;
 
 contract Chase {
-    uint256 public amount;
+    address public owner;
+    uint256 public totalAmount;
+    uint256 public totalClaimedCount;
+    uint256 public noOfClaims;
+    mapping(address => bool) public hasClaimed;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
 
     // Constructor sets initial deposit value
-    constructor() payable {
+    constructor(uint256 _noOfClaims) payable {
         require(msg.value > 0, "Initial deposit must be greater than zero");
-        amount = msg.value;
+        owner = msg.sender;
+        totalAmount = msg.value;
+        noOfClaims = _noOfClaims;
     }
 
     // Function to deposit ETH
-    function deposit() external payable {
-        amount += msg.value;
+    function deposit() external onlyOwner payable {
+        totalAmount += msg.value;
     }
 
     // Function to claim ETH
     function claim() external {
-        require(amount > 0, "No ETH available to claim");
+        uint256 availableClaims = noOfClaims - totalClaimedCount;
 
-        uint256 toTransfer = amount;
-        amount = 0;
+        require(availableClaims > 0, "No more claims available");
+        require(totalAmount > 0, "No ETH available to claim");
+        require(!hasClaimed[msg.sender], "You have already claimed");
+
+
+        uint256 toTransfer = totalAmount / (noOfClaims - totalClaimedCount);
+        totalAmount -= toTransfer;
+        totalClaimedCount++;
+        hasClaimed[msg.sender] = true;
         payable(msg.sender).transfer(toTransfer);
+    }
+
+    // Function to withdraw remaining ETH by the owner
+    function withdrawRemaining() external onlyOwner {
+        require(totalAmount > 0, "No ETH available to withdraw");
+        payable(owner).transfer(totalAmount);
+        totalAmount = 0;
     }
 }
